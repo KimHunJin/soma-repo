663c74b5da9f45de582b30632bc564169c583e38
KimHunJin <ysg01129@naver.com>
2018-08-08 20:19:01 +0900

    [date] 2018.08.08 (Wed.)
    [add]
    1. remix-ide analysis

--- vm/routes/ethereum/compile/compile.js
+++ vm/routes/ethereum/compile/compile.js
@@ -2,28 +2,37 @@
 
 var solc = require('solc');
 var fs = require('fs');
+var registry = require('../global/registry');
 
-fs.readFile('./contract/Casino.sol', 'utf8', function(err, data) {
-	if(err) {throw err};
-	solc.loadRemoteVersion('latest', function (err, solcSnapshot) {
-		if (err) {
+class Compile {
+
+	fs.readFile('./contract/Casino.sol', 'utf8', function(err, data) {
+		if(err) {throw err};
+		solc.loadRemoteVersion('latest', function (err, solcSnapshot) {
+			if (err) {
+				console.log(err);
 		// An error was encountered, display and quit
-		}
-
-		var p = new Promise(
-			function(res, rej) {
-				res(solcSnapshot.compile(data, 1));
 			}
-		);
-		p.then(
-			function(val) {
-				//TODO ; abi 추출
-//				console.log(val.contracts[0]);
-			});
-	});
-});
+
+			var p = new Promise(
+				function(res, rej) {
+					res(solcSnapshot.compile(data, 1));
+				}
+			);
+			p.then(
+				function(val) {
+					//TODO ; abi 추출
+					var bytecode = val.contracts[':Casino'].bytecode;
+					console.log(bytecode);
+
+					var abi = JSON.parse(val.contracts[':Casino'].interface);
+					console.log(abi);
+				}
+			);
+		});
+	});	
+}
 
 
-
-module.exports = fs;
+module.exports = Compile;
 
